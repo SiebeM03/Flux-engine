8f0e44ba5128775e5a4d5d1800df6d3b07db500e
Siebe Michiels <r0901813@student.thomasmore.be>
2026-02-08 22:39:02 +0100

    Moved (almost) all LWJGL dependencies to only be in flux-lwjgl rather than the root project

--- flux-util/src/main/java/me/siebe/flux/util/config/Flux.java
+++ flux-util/src/main/java/me/siebe/flux/util/config/Flux.java
@@ -1,10 +1,9 @@
 package me.siebe.flux.util.config;
 
-import org.lwjgl.system.MemoryUtil;
-
-import static org.lwjgl.glfw.GLFW.GLFW_DONT_CARE;
-
 public class Flux {
-    public static final int FLUX_DONT_CARE = GLFW_DONT_CARE;
-    public static final long NULL = MemoryUtil.NULL;
+    /** Taken from {@code org.lwjgl.glfw.GLFW.GLFW_DONT_CARE }. Might have to check other libraries such as SDL */
+    public static final int FLUX_DONT_CARE = -1;
+
+    /** Taken from {@code org.lwjgl.system.MemoryUtil }. Might have to check other libraries such as SDL */
+    public static final long NULL = 0L;
 }
