e4eab6827c04e3182474eac9abbab49401f37420
Siebe Michiels <r0901813@student.thomasmore.be>
2026-02-08 23:10:02 +0100

    Potential fix for pooled events being released more than once

--- flux-api/src/main/java/me/siebe/flux/api/event/Event.java
+++ flux-api/src/main/java/me/siebe/flux/api/event/Event.java
@@ -1,4 +1,7 @@
 package me.siebe.flux.api.event;
 
+import java.util.UUID;
+
 public abstract class Event {
+    public final UUID uuid = UUID.randomUUID();
 }
--- flux-core/src/main/java/me/siebe/flux/event/DefaultEventPool.java
+++ flux-core/src/main/java/me/siebe/flux/event/DefaultEventPool.java
@@ -5,11 +5,19 @@ import me.siebe.flux.api.event.EventPool;
 import me.siebe.flux.api.event.traits.Pooled;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Supplier;
 
 public final class DefaultEventPool<E extends Event & Pooled> implements EventPool<E> {
     private final ArrayDeque<E> pool = new ArrayDeque<>();
     private final Supplier<E> factory;
+
+    // Keeps a Map of all events currently in the pool, this allows for O(1) lookups when trying to reset an event
+    // If the event is already in the pool (and this map), it means this event was already releaded so calling
+    // release() will not do anything
+    private final Map<UUID, Event> eventsInPool = new HashMap<>();
 
     public DefaultEventPool(Supplier<E> factory) {
         this.factory = factory;
@@ -22,13 +30,16 @@ public final class DefaultEventPool<E extends Event & Pooled> implements EventPo
             event = factory.get();
         }
         event.reset();
+        eventsInPool.remove(event.uuid);
         return event;
     }
 
     @Override
     public void release(E event) {
-        // TODO prevent event from being released multiple times and thus also added to the queue multiple times
+        if (eventsInPool.containsKey(event.uuid)) return;
+
         event.reset();
+        eventsInPool.put(event.uuid, event);
         pool.addLast(event);
     }
 
